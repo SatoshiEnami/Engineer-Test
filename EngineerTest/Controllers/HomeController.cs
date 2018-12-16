770ba8b0a293c0447cd4e78847816f009e852716
satoshi enami <sato.0210.romanticistegoist@hotmail.co.jp>
2018-12-16 21:53:01 +0900

    Commit

---
+++ EngineerTest/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EngineerTest.Models;
+
+namespace EngineerTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private MvcBasicContext db = new MvcBasicContext();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult DataViewer()
+        {
+            return View();
+        }
+
+        /**public IActionResult List()
+        {
+            return View(db.Members);
+        }**/
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EngineerTest/Models/MvcBasicInitializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace EngineerTest.Models
+{
+    public class MvcBasicInitializer : DropCreateDatabaseAlways<MvcBasicContext>
+    {
+        protected override void Seed(MvcBasicContext context)
+        {
+            var members = new List<Member>
+            {
+                new Member
+                {
+                    Name="山田　太郎",
+                    Email = "example@exaple"
+                }
+            };
+            members.ForEach(m => context.Members.Add(m));
+            context.SaveChanges();
+        }
+    }
+}
